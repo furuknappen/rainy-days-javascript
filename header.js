745bc1927a917555e9bc16b53d58af8be6317409
Lillegul <camillal1@live.no>
2026-02-20 14:21:27 +0100

    fixed errors

--- header.js
+++ header.js
@@ -1,30 +1,19 @@
-'use strict';
-//TODO: make notifications show up on homepage too
+"use strict";
 
 export function rerenderCartNotification() {
   let cart = JSON.parse(localStorage.getItem("cart")) || [];
   const notification = document.querySelector(".cartNotification");
   notification.textContent = cart.length;
-  let notificationCartNumber = "";
-  console.log("cart length: " + cart.length);
-  console.log(cart);
-
-  if ((cart = [])) {
-    notification.computedStyleMap.display = "none";
+  if (cart.length == 0) {
+    notification.style.display = "none";
+  } else if (cart.length <= 9) {
+    notification.classList.add("under10");
+    notification.classList.remove("over10");
   }
-
-
-  if(cart.length <= 9 ){
-    notification.classList.add("under10")
-     notification.classList.remove("over10")
-  } 
-  // TODO: does not work
-  if(cart.length >= 10 ){
-    notification.classList.remove("under10")
-    notification.classList.add("over10")
-
-  } 
-
+  else {
+    notification.classList.remove("under10");
+    notification.classList.add("over10");
+  }
 }
-// rerenderCartNotification();
 
+rerenderCartNotification()
